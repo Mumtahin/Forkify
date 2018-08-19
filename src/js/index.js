c42de2258d0658dbe62cb9d4fca32a5b6374c21e
Mumtahin <n.mumtahin1@gmail.com>
2018-08-19 23:50:03 +0100

    add test api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
-import str from './models/Search';
+import axios from 'axios';
 
-import {add, mult, ID} from './views/searchView';
-
-console.log(`using imported functions! ${add(ID,2)} and ${mult(3,5)} and ${str}`);
+async function getResults(query) {
+    const key = 'fb5fd7827e0db097f01e0d2fa92874b1';
+    try {
+        const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+    
+}
+getResults('tomato pasta');
