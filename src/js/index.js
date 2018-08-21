b6ad2f9d0e64a7472e5ef766d764136aa4b736ac
Mumtahin <n.mumtahin1@gmail.com>
2018-08-22 00:06:28 +0100

    add search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = 'fb5fd7827e0db097f01e0d2fa92874b1';
-    try {
-        const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-    
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = 'fb5fd7827e0db097f01e0d2fa92874b1';
+        try {
+            const res = await axios(`http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
