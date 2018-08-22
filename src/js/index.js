aeaafa25eba9e72c70b871fceab4eca340a8c91e
Mumtahin <n.mumtahin1@gmail.com>
2018-08-23 00:53:56 +0100

    add search controller structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/* global state of the app
+    - search object
+    - current recipe object
+    - shopping list object
+    - liked recipes
+*/
+const state = {}
+
+const controlSearch = async () => {
+    // 1) get query from view
+    const query = 'pizza';
+
+    if (query) {
+        // 2) create new search object, add to state
+        state.search = new Search(query);
+
+        // 3) prepare UI for results
+
+        // 4) search for recipes
+        await state.search.getResults();
+
+        // 5) render results on UI
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
